6715bc68939264f4f119dd10e231db2651ef4812
Ajay krishnan creations <118874449+AjayKrishnanKen42@users.noreply.github.com>
2023-11-08 15:49:16 +0530

    video.js

--- src/Application/Video/videoPlayer.tsx
+++ src/Application/Video/videoPlayer.tsx
@@ -1,48 +1,69 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { getVideos } from "../../ApiService/ApiServices";
-const video = require("../../Assets/sample.mp4");
+import { getVideos } from '../../ApiService/ApiServices';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
 interface Video {
   _id: string;
   title: string;
   video: string;
   __v: number;
 }
+
 const VideoPlayer: React.FC = () => {
   const [videos, setVideos] = useState<Video[]>([]);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const videoUrl = '/sample.mp4';
   useEffect(() => {
     const fetchVideos = async () => {
       try {
         const response = await getVideos();
         setVideos(response);
       } catch (error) {
-        console.error("Error fetching videos:", error);
+        console.error('Error fetching videos:', error);
       }
     };
     fetchVideos();
+    return () => {
+      if (videoRef.current) {
+        videojs(videoRef.current).dispose();
+      }
+    };
   }, []);
+  useEffect(() => {
+    if (videoRef.current) {
+      videojs(videoRef.current);
+    }
+  }, [videos]);
   return (
     <div className='video-screen-background'>
       <div className="background-video-container">
-      <video autoPlay loop muted className="background-video">
-        <source src={video} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div className="text-overlay learning-content" style={{paddingTop:"500px"}}>
-        <h1>Welcome to Learning module</h1>
-        <p>You can access all the learning content here</p>
-      </div>
-    </div>
-    <p className='video-heading'>Based on your Search History</p>
-    <div className='row'>
-      {videos.map((video, index) => (
-        <div key={video._id} className='col-3'>
-          <center>
-          <video src={`http://localhost:4000/${video.video}`} controls className="video-js" /><br/>
-          <b style={{padding:"10px",color:"white"}}>{video.title}</b>
-          </center>
+        <video autoPlay loop muted className="background-video">
+          <source src={videoUrl} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="text-overlay learning-content" style={{ paddingTop: "500px" }}>
+          <h1>Welcome to Learning module</h1>
+          <p>You can access all the learning content here</p>
         </div>
-      ))}
-    </div>
+      </div>
+      <p className='video-heading'>Based on your Search History</p>
+      <div className='row'>
+        {videos.map((video, index) => (
+          <div key={video._id} className='col-3'>
+            <center>
+              <video
+                ref={videoRef}
+                data-setup='{}'
+                className="video-js vjs-default-skin"
+                controls
+              >
+                <source src={`http://localhost:4000/${video.video}`} type="video/mp4" />
+              </video>
+              <b style={{ padding: "10px", color: "white" }}>{video.title}</b>
+            </center>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
